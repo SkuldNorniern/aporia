1d6408fb24b4f47665209e37540781c350800fb0
SkuldNorniern <skuldnorniern@gmail.com>
2024-11-11 13:45:13 +0900

    feat: add example contains all

--- examples/total.rs
+++ examples/total.rs
@@ -1,4 +1,7 @@
-use alea::{backend::XorShift, backend::LCG, backend::PCG, Rng};
+use alea::{
+    backend::{MT19937_64, PCG, XorShift, LCG, SplitMix64, Xoshiro256StarStar},
+    Rng,
+};
 
 fn main() {
     // Using PCG
@@ -13,7 +16,22 @@ fn main() {
     let lcg = LCG::new(12345);
     let mut rng3 = Rng::new(lcg);
 
+    // Using MT19937_64
+    let mt19937_64 = MT19937_64::new(12345);
+    let mut rng4 = Rng::new(mt19937_64);
+
+    // Using SplitMix64
+    let splitmix64 = SplitMix64::new(12345);
+    let mut rng5 = Rng::new(splitmix64);
+
+    // Using Xoshiro256StarStar
+    let xoshiro256starstar = Xoshiro256StarStar::new(12345);
+    let mut rng6 = Rng::new(xoshiro256starstar);
+
     println!("PCG: {}", rng1.next_u64());
     println!("XorShift: {}", rng2.next_u64());
     println!("LCG: {}", rng3.next_u64());
+    println!("MT19937_64: {}", rng4.next_u64());
+    println!("SplitMix64: {}", rng5.next_u64());
+    println!("Xoshiro256StarStar: {}", rng6.next_u64());
 }
